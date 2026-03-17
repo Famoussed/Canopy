3da6c49b5b717609526eaa00f941136002087ea3
ahsecek <ahsecek@gmail.com>
2026-03-17 14:16:20 +0300

    MVP Commiti

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,17 +4,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Events\Project\MemberAdded;
-use App\Events\Scrum\SprintClosed;
 use App\Events\Scrum\StoryStatusChanged;
-use App\Events\Scrum\TaskAssigned;
 use App\Events\Scrum\TaskStatusChanged;
-use App\Listeners\RecalculateEpicCompletion;
-use App\Listeners\ReturnUnfinishedStoriesToBacklog;
-use App\Listeners\SendMemberAddedNotification;
 use App\Listeners\SendStatusChangeNotification;
-use App\Listeners\SendTaskAssignedNotification;
-use App\Listeners\UpdateBurndownSnapshot;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
@@ -38,16 +30,10 @@ class AppServiceProvider extends ServiceProvider
         Model::preventLazyLoading(! app()->isProduction());
         Model::preventSilentlyDiscardingAttributes(! app()->isProduction());
 
-        Event::listen(StoryStatusChanged::class, RecalculateEpicCompletion::class);
         Event::listen(StoryStatusChanged::class, SendStatusChangeNotification::class);
-        Event::listen(StoryStatusChanged::class, UpdateBurndownSnapshot::class);
 
         Event::listen(TaskStatusChanged::class, SendStatusChangeNotification::class);
+        Event::listen(\App\Events\Issue\IssueStatusChanged::class, SendStatusChangeNotification::class);
 
-        Event::listen(TaskAssigned::class, SendTaskAssignedNotification::class);
-
-        Event::listen(MemberAdded::class, SendMemberAddedNotification::class);
-
-        Event::listen(SprintClosed::class, ReturnUnfinishedStoriesToBacklog::class);
     }
 }
